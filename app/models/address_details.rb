2bbfa249541c97abf57e14cdd688763b9d6333b9
devarasu <devarasutesting@gmail.com>
2012-08-22 14:59:34 +0530

    files removed

--- app/models/address_details.rb
+++
@@ -1,7 +0,0 @@
-class AddressDetails < ActiveRecord::Base
-  #-------------------------------------ASSOCIATION BLOCK--------------------------------
-  belongs_to :addressable, :polymorphic => true
-  belongs_to :city
-  belongs_to :country
-  belongs_to :state
-end
