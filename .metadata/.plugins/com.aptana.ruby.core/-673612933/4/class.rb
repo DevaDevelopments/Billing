2bc3534d404b573e83f4e1e16edf4f69ef61c256
abaranji <abaranjir@yahoo.com>
2012-08-22 20:17:58 +0530

    employee

---
+++ .metadata/.plugins/com.aptana.ruby.core/-673612933/4/class.rb
@@ -0,0 +1,123 @@
+class Class < Module
+
+  ARGF = ARGF
+  ARGV = ["/home/abaranji/Aptana Studio 3 Workspace/.metadata/.plugins/com.aptana.ruby.core/-673612933/4"]
+  ArgumentError = ArgumentError
+  Array = Array
+  Bignum = Bignum
+  Binding = Binding
+  Class = Class
+  Comparable = Comparable
+  Continuation = Continuation
+  Data = Data
+  Dir = Dir
+  ENV = {"GNOME_KEYRING_CONTROL"=>"/tmp/keyring-4kkIWh", "GTK_MODULES"=>"canberra-gtk-module", "SHLVL"=>"1", "SSH_AGENT_PID"=>"1544", "MOZILLA_FIVE_HOME"=>"/usr/lib64/xulrunner-1.9.2.25", "rvm_version"=>"1.10.2", "__array_start"=>"0", "SESSION_MANAGER"=>"local/abaranji:@/tmp/.ICE-unix/1514,unix/abaranji:/tmp/.ICE-unix/1514", "GNOME_DESKTOP_SESSION_ID"=>"this-is-deprecated", "GDMSESSION"=>"gnome", "XDG_SESSION_COOKIE"=>"5b4f88e4b4d6fde75e4466c600000008-1337661898.41452-368769156", "XDG_DATA_DIRS"=>"/usr/share/gnome:/usr/local/share/:/usr/share/", "MANDATORY_PATH"=>"/usr/share/gconf/gnome.mandatory.path", "PWD"=>"/home/abaranji/Abaranji Files/Aptana_Studio_3", "rvm_prefix"=>"/home/abaranji", "LOGNAME"=>"abaranji", "SSH_AUTH_SOCK"=>"/tmp/keyring-4kkIWh/ssh", "LD_LIBRARY_PATH"=>"/usr/lib/jvm/java-6-openjdk/jre/lib/amd64/server:/usr/lib/jvm/java-6-openjdk/jre/lib/amd64:/usr/lib/jvm/java-6-openjdk/jre/../lib/amd64", "rvm_hooks_path"=>"/home/abaranji/.rvm/hooks", "DBUS_SESSION_BUS_ADDRESS"=>"unix:abstract=/tmp/dbus-yvDhZMviBC,guid=6f015d21e74baddd545ae02b00000023", "SHELL"=>"/bin/bash", "GDM_KEYBOARD_LAYOUT"=>"us", "rvm_bin_path"=>"/home/abaranji/.rvm/bin", "GNOME_KEYRING_PID"=>"1495", "escape_flag"=>"1", "_first"=>"0", "XDG_CONFIG_DIRS"=>"/etc/xdg/xdg-gnome:/etc/xdg", "PATH"=>"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/abaranji/.rvm/bin", "_second"=>"1", "DESKTOP_SESSION"=>"gnome", "APTANA_VERSION"=>"3.1.2.201205041324", "DISPLAY"=>":0.0", "USER"=>"abaranji", "rvm_rubygems_path"=>"/home/abaranji/.rvm/rubygems", "HOME"=>"/home/abaranji", "rvm_path"=>"/home/abaranji/.rvm", "XAUTHORITY"=>"/var/run/gdm/auth-for-abaranji-FVYDR5/database", "GDM_LANG"=>"en_US.utf8", "WINDOWPATH"=>"7", "DEFAULTS_PATH"=>"/usr/share/gconf/gnome.default.path", "USERNAME"=>"abaranji", "ORBIT_SOCKETDIR"=>"/tmp/orbit-abaranji", "rvm_reload_flag"=>"0", "LANG"=>"en_US.utf8"}
+  EOFError = EOFError
+  Enumerable = Enumerable
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  MatchingData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  NIL = nil
+  NameError = NameError
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/home/abaranji/Aptana Studio 3 Workspace/.metadata/.plugins/com.aptana.ruby.core/-673612933/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  PLATFORM = "x86_64-linux"
+  Precision = Precision
+  Proc = Proc
+  Process = Process
+  RELEASE_DATE = "2010-06-23"
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2010 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 1.8.7 (2010-06-23 patchlevel 299) [x86_64-linux]"
+  RUBY_PATCHLEVEL = 299
+  RUBY_PLATFORM = "x86_64-linux"
+  RUBY_RELEASE_DATE = "2010-06-23"
+  RUBY_VERSION = "1.8.7"
+  Range = Range
+  RangeError = RangeError
+  Regexp = Regexp
+  RegexpError = RegexpError
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScriptError = ScriptError
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TOPLEVEL_BINDING = #<Binding:0x7f43d2b07f80>
+  TRUE = true
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UnboundMethod = UnboundMethod
+  VERSION = "1.8.7"
+  ZeroDivisionError = ZeroDivisionError
+
+
+  def allocate
+  end
+
+  def new(arg0, arg1, *rest)
+  end
+
+  def superclass
+  end
+
+
+  protected
+
+
+  private
+
+  def inherited(arg0)
+  end
+
+  def initialize(arg0, arg1, *rest)
+  end
+
+  def initialize_copy(arg0)
+  end
+
+end
---
+++ .metadata/.plugins/com.aptana.ruby.core/-673612933/4/module.rb
@@ -0,0 +1,300 @@
+class Module < Object
+
+  ARGF = ARGF
+  ARGV = ["/home/abaranji/Aptana Studio 3 Workspace/.metadata/.plugins/com.aptana.ruby.core/-673612933/4"]
+  ArgumentError = ArgumentError
+  Array = Array
+  Bignum = Bignum
+  Binding = Binding
+  Class = Class
+  Comparable = Comparable
+  Continuation = Continuation
+  Data = Data
+  Dir = Dir
+  ENV = {"GNOME_KEYRING_CONTROL"=>"/tmp/keyring-4kkIWh", "GTK_MODULES"=>"canberra-gtk-module", "SHLVL"=>"1", "SSH_AGENT_PID"=>"1544", "MOZILLA_FIVE_HOME"=>"/usr/lib64/xulrunner-1.9.2.25", "rvm_version"=>"1.10.2", "__array_start"=>"0", "SESSION_MANAGER"=>"local/abaranji:@/tmp/.ICE-unix/1514,unix/abaranji:/tmp/.ICE-unix/1514", "GNOME_DESKTOP_SESSION_ID"=>"this-is-deprecated", "GDMSESSION"=>"gnome", "XDG_SESSION_COOKIE"=>"5b4f88e4b4d6fde75e4466c600000008-1337661898.41452-368769156", "XDG_DATA_DIRS"=>"/usr/share/gnome:/usr/local/share/:/usr/share/", "MANDATORY_PATH"=>"/usr/share/gconf/gnome.mandatory.path", "PWD"=>"/home/abaranji/Abaranji Files/Aptana_Studio_3", "rvm_prefix"=>"/home/abaranji", "LOGNAME"=>"abaranji", "SSH_AUTH_SOCK"=>"/tmp/keyring-4kkIWh/ssh", "LD_LIBRARY_PATH"=>"/usr/lib/jvm/java-6-openjdk/jre/lib/amd64/server:/usr/lib/jvm/java-6-openjdk/jre/lib/amd64:/usr/lib/jvm/java-6-openjdk/jre/../lib/amd64", "rvm_hooks_path"=>"/home/abaranji/.rvm/hooks", "DBUS_SESSION_BUS_ADDRESS"=>"unix:abstract=/tmp/dbus-yvDhZMviBC,guid=6f015d21e74baddd545ae02b00000023", "SHELL"=>"/bin/bash", "GDM_KEYBOARD_LAYOUT"=>"us", "rvm_bin_path"=>"/home/abaranji/.rvm/bin", "GNOME_KEYRING_PID"=>"1495", "escape_flag"=>"1", "_first"=>"0", "XDG_CONFIG_DIRS"=>"/etc/xdg/xdg-gnome:/etc/xdg", "PATH"=>"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/home/abaranji/.rvm/bin", "_second"=>"1", "DESKTOP_SESSION"=>"gnome", "APTANA_VERSION"=>"3.1.2.201205041324", "DISPLAY"=>":0.0", "USER"=>"abaranji", "rvm_rubygems_path"=>"/home/abaranji/.rvm/rubygems", "HOME"=>"/home/abaranji", "rvm_path"=>"/home/abaranji/.rvm", "XAUTHORITY"=>"/var/run/gdm/auth-for-abaranji-FVYDR5/database", "GDM_LANG"=>"en_US.utf8", "WINDOWPATH"=>"7", "DEFAULTS_PATH"=>"/usr/share/gconf/gnome.default.path", "USERNAME"=>"abaranji", "ORBIT_SOCKETDIR"=>"/tmp/orbit-abaranji", "rvm_reload_flag"=>"0", "LANG"=>"en_US.utf8"}
+  EOFError = EOFError
+  Enumerable = Enumerable
+  Errno = Errno
+  Etc = Etc
+  Exception = Exception
+  FALSE = false
+  FalseClass = FalseClass
+  File = File
+  FileTest = FileTest
+  FileUtils = FileUtils
+  Fixnum = Fixnum
+  Float = Float
+  FloatDomainError = FloatDomainError
+  GC = GC
+  Hash = Hash
+  IO = IO
+  IOError = IOError
+  IndexError = IndexError
+  Integer = Integer
+  Interrupt = Interrupt
+  Kernel = Kernel
+  LoadError = LoadError
+  LocalJumpError = LocalJumpError
+  Marshal = Marshal
+  MatchData = MatchData
+  MatchingData = MatchData
+  Math = Math
+  Method = Method
+  Module = Module
+  NIL = nil
+  NameError = NameError
+  NilClass = NilClass
+  NoMemoryError = NoMemoryError
+  NoMethodError = NoMethodError
+  NotImplementedError = NotImplementedError
+  Numeric = Numeric
+  OUTPUT_PATH = "/home/abaranji/Aptana Studio 3 Workspace/.metadata/.plugins/com.aptana.ruby.core/-673612933/4/"
+  Object = Object
+  ObjectSpace = ObjectSpace
+  PLATFORM = "x86_64-linux"
+  Precision = Precision
+  Proc = Proc
+  Process = Process
+  RELEASE_DATE = "2010-06-23"
+  RUBY_COPYRIGHT = "ruby - Copyright (C) 1993-2010 Yukihiro Matsumoto"
+  RUBY_DESCRIPTION = "ruby 1.8.7 (2010-06-23 patchlevel 299) [x86_64-linux]"
+  RUBY_PATCHLEVEL = 299
+  RUBY_PLATFORM = "x86_64-linux"
+  RUBY_RELEASE_DATE = "2010-06-23"
+  RUBY_VERSION = "1.8.7"
+  Range = Range
+  RangeError = RangeError
+  Regexp = Regexp
+  RegexpError = RegexpError
+  RuntimeError = RuntimeError
+  STDERR = IO.new
+  STDIN = IO.new
+  STDOUT = IO.new
+  ScriptError = ScriptError
+  SecurityError = SecurityError
+  Signal = Signal
+  SignalException = SignalException
+  StandardError = StandardError
+  StopIteration = StopIteration
+  String = String
+  Struct = Struct
+  Symbol = Symbol
+  SyntaxError = SyntaxError
+  SystemCallError = SystemCallError
+  SystemExit = SystemExit
+  SystemStackError = SystemStackError
+  TOPLEVEL_BINDING = #<Binding:0x7f43d2b07f80>
+  TRUE = true
+  Thread = Thread
+  ThreadError = ThreadError
+  ThreadGroup = ThreadGroup
+  Time = Time
+  TrueClass = TrueClass
+  TypeError = TypeError
+  UnboundMethod = UnboundMethod
+  VERSION = "1.8.7"
+  ZeroDivisionError = ZeroDivisionError
+
+  def self.constants
+  end
+
+  def self.nesting
+  end
+
+
+  def <(arg0)
+  end
+
+  def <=(arg0)
+  end
+
+  def <=>(arg0)
+  end
+
+  def ==(arg0)
+  end
+
+  def ===(arg0)
+  end
+
+  def >(arg0)
+  end
+
+  def >=(arg0)
+  end
+
+  def ancestors
+  end
+
+  def autoload(arg0, arg1)
+  end
+
+  def autoload?(arg0)
+  end
+
+  def class_eval(arg0, arg1, *rest)
+  end
+
+  def class_exec(arg0, arg1, *rest)
+  end
+
+  def class_variable_defined?(arg0)
+  end
+
+  def class_variables
+  end
+
+  def const_defined?(arg0)
+  end
+
+  def const_get(arg0)
+  end
+
+  def const_missing(arg0)
+  end
+
+  def const_set(arg0, arg1)
+  end
+
+  def constants
+  end
+
+  def freeze
+  end
+
+  def include?(arg0)
+  end
+
+  def included_modules
+  end
+
+  def instance_method(arg0)
+  end
+
+  def instance_methods(arg0, arg1, *rest)
+  end
+
+  def method_defined?(arg0)
+  end
+
+  def module_eval(arg0, arg1, *rest)
+  end
+
+  def module_exec(arg0, arg1, *rest)
+  end
+
+  def name
+  end
+
+  def private_class_method(arg0, arg1, *rest)
+  end
+
+  def private_instance_methods(arg0, arg1, *rest)
+  end
+
+  def private_method_defined?(arg0)
+  end
+
+  def protected_instance_methods(arg0, arg1, *rest)
+  end
+
+  def protected_method_defined?(arg0)
+  end
+
+  def public_class_method(arg0, arg1, *rest)
+  end
+
+  def public_instance_methods(arg0, arg1, *rest)
+  end
+
+  def public_method_defined?(arg0)
+  end
+
+  def to_s
+  end
+
+
+  protected
+
+
+  private
+
+  def alias_method(arg0, arg1)
+  end
+
+  def append_features(arg0)
+  end
+
+  def attr(arg0, arg1, *rest)
+  end
+
+  def attr_accessor(arg0, arg1, *rest)
+  end
+
+  def attr_reader(arg0, arg1, *rest)
+  end
+
+  def attr_writer(arg0, arg1, *rest)
+  end
+
+  def class_variable_get(arg0)
+  end
+
+  def class_variable_set(arg0, arg1)
+  end
+
+  def define_method(arg0, arg1, *rest)
+  end
+
+  def extend_object(arg0)
+  end
+
+  def extended(arg0)
+  end
+
+  def include(arg0, arg1, *rest)
+  end
+
+  def included(arg0)
+  end
+
+  def initialize
+  end
+
+  def initialize_copy(arg0)
+  end
+
+  def method_added(arg0)
+  end
+
+  def method_removed(arg0)
+  end
+
+  def method_undefined(arg0)
+  end
+
+  def module_function(arg0, arg1, *rest)
+  end
+
+  def private(arg0, arg1, *rest)
+  end
+
+  def protected(arg0, arg1, *rest)
+  end
+
+  def public(arg0, arg1, *rest)
+  end
+
+  def remove_class_variable(arg0)
+  end
+
+  def remove_const(arg0)
+  end
+
+  def remove_method(arg0, arg1, *rest)
+  end
+
+  def undef_method(arg0, arg1, *rest)
+  end
+
+end
