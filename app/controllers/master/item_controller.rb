00ab8b4bac206fb5ce3fa5538793d3dcba5b3c73
devarasu <adevarasu@gmail.com>
2012-08-25 02:21:15 +0530

    auto generation id created

--- app/controllers/master/item_controller.rb
+++ app/controllers/master/item_controller.rb
@@ -1,6 +1,7 @@
 class Master::ItemController < ApplicationController
-    
+    # include CommanMethods
   def index
+    @item = Item.new
     
   end
   
@@ -9,23 +10,31 @@ class Master::ItemController < ApplicationController
   end
   
   def create
-    
+    @item = Item.new(params[:item])
+    if @item.save
+      @item.update_code
+      redirect_to :action => 'index',:notice=> @item.item_name + " Created Successfully"
+    end
   end
   
   def edit
-    
+    @item = Item.find(params[:id])
+    @action = 'update'
+    render :action=> 'index'
   end
   
   def update
-    
+    @itemm = Item.find(params[:id])
+    if @itemm.update_attributes(params[:item])
+     render :action => 'index'
+    end
   end
   
-  def show
-    
-  end
-  
-  def destroy_item
-    
-  end
+ def destroy_item
+   @item = Item.find(params[:item_id])
+   @item.destroy
+   flash[:notice] = "Item removed sucessfully"
+   render :action=> 'index'
+ end
   
 end
